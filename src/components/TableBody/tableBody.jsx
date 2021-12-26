25b0e11ca80d252b4bc63b8edd76fbd93e496406
Maker0N <makerov@e1.ru>
2021-12-26 22:41:11 +0500

    fix delete

--- src/components/TableBody/tableBody.jsx
+++ src/components/TableBody/tableBody.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const TableBody = ({
-  data, columns,
+  data, columns, currentPage, usersCrop, handlePageChange,
 }) => {
   const renderContent = (item, column) => {
     if (columns[column].component) {
       const { component } = columns[column]
       if (typeof component === 'function') {
-        return component(item)
+        return component(item, currentPage, usersCrop, handlePageChange)
       }
       return component
     }
@@ -54,6 +54,17 @@ TableBody.propTypes = {
   columns: PropTypes.objectOf(PropTypes.objectOf(PropTypes
     .oneOfType([PropTypes.string, PropTypes.func, PropTypes.bool])))
     .isRequired,
+  currentPage: PropTypes.number.isRequired,
+  usersCrop: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    profession: PropTypes.objectOf(PropTypes.string),
+    qualities: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+    completedMeetings: PropTypes.number,
+    rate: PropTypes.number,
+    bookmark: PropTypes.bool,
+  })).isRequired,
+  handlePageChange: PropTypes.func.isRequired,
 }
 
 export default TableBody
